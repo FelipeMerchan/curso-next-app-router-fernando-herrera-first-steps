3819cc66cadddb1367a60df5c99fa2029118442c
Felipe Merchan <felipemerchanfuquen@gmail.com>
2024-03-24 01:33:24 -0500

    19. Next/Link

--- app/(general)/about/page.tsx
+++ app/(general)/about/page.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: 'SEO Title',
-  description: 'SEO Title',
+  title: 'About',
+  description: 'About page',
   keywords: ['About page, Information']
 };
 
