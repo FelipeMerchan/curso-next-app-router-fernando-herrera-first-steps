fc23b208c09bb58e4a68f75e3d22fb4f3399a526
Felipe Merchan <felipemerchanfuquen@gmail.com>
2024-03-24 23:40:08 -0500

    31. docker - construcción recomendada

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  /* Cuando estamos creando imágenes en Docker es recomendado
+  usar el output standalone: */
+  output: "standalone",
+};
 
 export default nextConfig;
